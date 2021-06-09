b3b78cc00689f47cab483bd082dca86dc94d0eb0
yrc-kuyee <254626099@qq.com>
2021-06-09 16:17:08 +0800

    for ubuntu

---
+++ build/CMakeFiles/yolov5.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yrc/文档/Project/yolov5ncnn/src/YoloV5.cpp" "/home/yrc/文档/Project/yolov5ncnn/build/CMakeFiles/yolov5.dir/src/YoloV5.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/yrc/opencvlib/include/opencv4"
+  "/home/yrc/文档/ncnn/ncnn/build/install/include/ncnn"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/yolov5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/yolov5"
+  "../bin/yolov5.pdb"
+  "CMakeFiles/yolov5.dir/src/YoloV5.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolov5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
